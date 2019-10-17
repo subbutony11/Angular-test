bdf07510ac26d2638f34300fb4445c4fb469931e
Murad Ali Jamali <mjamali@csquareonline.local>
2019-10-17 14:34:04 +0500

    abcd

--- deploy/start-service.sh
+++ deploy/start-service.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+unlink /etc/nginx/sites-enabled/default
 cd /home/ec2-user/app/
+mv Node-proxy.conf  /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
 ng serve
 npm start >/dev/null 2>&1 &
