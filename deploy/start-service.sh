add9ba95f04319235415496ea7589aafdbf63ddf
Murad Ali Jamali <mjamali@csquareonline.local>
2019-10-17 19:28:17 +0500

    start file change

--- deploy/start-service.sh
+++ deploy/start-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 unlink /etc/nginx/sites-enabled/default
-cd /home/ec2-user/app/
+cd /var/www/html/
 mv Node-proxy.conf  /etc/nginx/sites-available
-ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
+ln -s /etc/nginx/sites-available/Node-proxy.con /etc/nginx/sites-enabled/Node-proxy.con
 ng serve
 npm start >/dev/null 2>&1 &
