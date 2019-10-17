467680c232233d7f05ddd99b23a7a47708eadbf6
Murad Ali Jamali <mjamali@csquareonline.local>
2019-10-17 19:47:02 +0500

    ;

--- deploy/start-service.sh
+++ deploy/start-service.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-unlink /etc/nginx/sites-enabled/default
 cd /var/www/html/
-mv Node-proxy.conf  /etc/nginx/sites-available
-ln -s /etc/nginx/sites-available/Node-proxy.con /etc/nginx/sites-enabled/Node-proxy.con
+mv Node-proxy.conf  /etc/nginx/conf.d
 ng serve
 npm start >/dev/null 2>&1 &
